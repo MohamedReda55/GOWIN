7121c596c8b212df767cd982d560eb48563b9b1b
MohamedReda55 <mohamed.reda7747@gmail.com>
2024-05-11 00:47:24 +0300

    final commit

---
+++ test_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <numwin.h>
+#include <math.h>
+
+int sum(int num1 , int num2 ){
+    return num1+num2;
+}
+
+int main(){
+
+    int result=sum(5,6);
+    
+    لو(result==11){
+        اطبع("sum: %d",result);
+    }
+
+    بينما(result!=0){
+        result--;
+    }
+    list li1=[1,2,3,4];
+    str Cstring="hello world";
+    println(li1,Cstring);
+
+
+}
